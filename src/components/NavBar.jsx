1cfd6e5e38515e5c7c7c3415eee255566f04653c
zruixuan <2544067255@qq.com>
2026-06-01 18:12:35 +0800

    revise Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,7 @@
 
 import styles from "../pages/Dashboard.module.css";
 import logo from "../assets/icon.png";
+import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
@@ -20,15 +21,56 @@ function NavBar() {
       {/* Navigation Links */}
       <nav className={styles.navLinks}>
   
-  <div className={`${styles.navItem} ${styles.active}`}>
-    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><rect x="3" y="3" width="7" height="9"></rect><rect x="14" y="3" width="7" height="5"></rect><rect x="14" y="12" width="7" height="9"></rect><rect x="3" y="16" width="7" height="5"></rect></svg>
+  <NavLink
+    to="/dashboard"
+    className={({ isActive }) =>
+      `${styles.navItem} ${isActive ? styles.active : ""}`
+    }
+  >
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <rect x="3" y="3" width="7" height="9"></rect>
+      <rect x="14" y="3" width="7" height="5"></rect>
+      <rect x="14" y="12" width="7" height="9"></rect>
+      <rect x="3" y="16" width="7" height="5"></rect>
+    </svg>
+
     Dashboard
-  </div>
+  </NavLink>
   
-  <div className={styles.navItem}>
-    <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="16" y1="13" x2="8" y2="13"></line><line x1="16" y1="17" x2="8" y2="17"></line><polyline points="10 9 9 9 8 9"></polyline></svg>
+  <NavLink
+    to="/eisenhower"
+    className={({ isActive }) =>
+      `${styles.navItem} ${isActive ? styles.active : ""}`
+    }
+  >
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+      <polyline points="14 2 14 8 20 8"></polyline>
+      <line x1="16" y1="13" x2="8" y2="13"></line>
+      <line x1="16" y1="17" x2="8" y2="17"></line>
+      <polyline points="10 9 9 9 8 9"></polyline>
+    </svg>
+    
     Tasks
-  </div>
+  </NavLink>
   
   <div className={styles.navItem}>
     <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><circle cx="12" cy="12" r="10"></circle><polyline points="12 6 12 12 16 14"></polyline></svg>
