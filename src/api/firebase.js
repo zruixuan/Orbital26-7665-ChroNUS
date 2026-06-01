163a4459efbb513f78eafe6d881be7d6169acf1b
ZhangHanzhongFox <e1513113@u.nus.edu>
2026-06-01 11:42:08 +0800

    feat(dashboard): integrate Auth and Firestore for data isolation
    feat: bind new tasks to user UID (User-Data Binding)
    
    feat: filter timeline data based on current logged-in user

--- src/api/firebase.js
+++ src/api/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDu5kDGUt7Jnxj5TWpyiWfsdTPNT6TH88g",
@@ -13,3 +14,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+export const db = getFirestore(app);
